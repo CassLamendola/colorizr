87c08eecd88e0a81233d73fd85ea99dfed2a8323
Cassandra Lamendola <cass.lamendola@gmail.com>
2016-04-10 15:55:11 -0700

    Add method to print a sample of each color

--- lib/colorizr.rb
+++ lib/colorizr.rb
@@ -2,9 +2,7 @@ class String
 	def self.color_codes
 		{
 			black: 30, red: 31, green: 32, yellow: 33, blue: 34,
-			magenta: 35, cyan: 36, light_gray: 37, dark_gray: 90,
-			light_red: 91, light_green: 92, light_yellow: 93,
-			light_blue: 94, light_magenta: 95, light_cyan: 96, white: 97
+			pink: 35, light_gray: 37, light_blue: 94, white: 97
 		}
 	end
 
@@ -15,17 +13,22 @@ class String
 		end
 	end
 
-	# gives an example of each color
-	def sample_colors
-	end
-
 	# generates colors
 	def self.create_colors
 		color_codes.each do |key, value|
 			self.send(:define_method, "#{key}") do
-				return "\e[#{value}m#{self}\e[0m"
+				"\e[#{value}m#{self}\e[0m"
 			end
 		end
 	end
 	create_colors
+
+	# gives an example of each color
+	instance_eval do
+		def sample_colors
+			color_codes.each do |key, value|
+				puts "This is \e[#{value}m#{key}\e[0m."
+			end
+		end
+	end
 end
