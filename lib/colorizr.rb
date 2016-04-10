787664f095e17db07fe4556b5df1d27a0a97546d
Cassandra Lamendola <cass.lamendola@gmail.com>
2016-04-10 15:24:31 -0700

    Create methods for all colors

--- lib/colorizr.rb
+++ lib/colorizr.rb
@@ -1,7 +1,16 @@
 class String
+	def self.color_codes
+		{
+			black: 30, red: 31, green: 32, yellow: 33, blue: 34,
+			magenta: 35, cyan: 36, light_gray: 37, dark_gray: 90,
+			light_red: 91, light_green: 92, light_yellow: 93,
+			light_blue: 94, light_magenta: 95, light_cyan: 96, white: 97
+		}
+	end
+
 	# lists all available colors
 	instance_eval do
-		def self.colors
+		def colors
 			color_codes.keys
 		end
 	end
@@ -11,21 +20,12 @@ class String
 	end
 
 	# generates colors
-	def create_colors
+	def self.create_colors
+		color_codes.each do |key, value|
+			self.send(:define_method, "#{key}") do
+				return "\e[#{value}m#{self}\e[0m"
+			end
+		end
 	end
-
-	private
-
-	def self.color_codes
-		{
-			black: 30, red: 31, green: 32, yellow: 33, blue: 34,
-			magenta: 35, cyan: 36, light_gray: 37, dark_gray: 90,
-			light_red: 91, light_green: 92, light_yellow: 93,
-			light_blue: 94, light_magenta: 95, light_cyan: 96, white: 97
-		}
-	end
+	create_colors
 end
-
-#a = String.new("hello")
-#puts a.colors
-puts String.colors
